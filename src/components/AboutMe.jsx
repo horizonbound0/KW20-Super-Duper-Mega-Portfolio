904f538436541c76edbf8798f95ec83acb449bf0
Ken W <horizonbound0@gmail.com>
2024-11-21 17:06:45 -0600

    redesigning a few things in prep for state machine

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,15 +7,15 @@ export default function AboutMe(props) {
     }
     return (
         <div className="border media container mx-auto my-2">
-            <div className="row"></div>
-            <img style={styles.image} src="./assets/profilePic.jpg" alt="Ken Wagner" className="col-sm col-4" />
-            <div className="media-body col-sm col-8">
-                <h1>About Me</h1>
-                <p>
-                Full-Stack Web Developer, with a background in IT Support and Video Game Design. I graduated from Full Sail University with a Bachelor’s Degree in Video Game Design, and a certificate for full stack web development from University of Texas, Austin. Creative and dedicated problem solver with a love of developing and improving useful apps with clean and extendible code.
-                </p>
-            </div>
-            
+            <div className="row p-2">
+                <img style={styles.image} src="./assets/profilePic.jpg" alt="Ken Wagner" className="#" />
+                <div className="col">
+                    <h1 className="#">About Me</h1>
+                    <p className="#">
+                    Full-Stack Web Developer, with a background in IT Support and Video Game Design. I graduated from Full Sail University with a Bachelor’s Degree in Video Game Design, and a certificate for full stack web development from University of Texas, Austin. Creative and dedicated problem solver with a love of developing and improving useful apps with clean and extendible code.
+                    </p>
+                </div>
+            </div>            
         </div>
     )
 }
