b1cf3ccf1fc01ddf703fce4f5477e29cfd39517b
Ken W <horizonbound0@gmail.com>
2024-11-21 13:53:10 -0600

    added profile picture, running build soon to test.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe(props) {
     console.log(props);
     return (
         <div className="border">
-            <img src="#" alt="Ken Wagner" />
+            <img src="../assets/profilePic.jpg" alt="Ken Wagner" />
             <h1>About Me</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ipsum nobis aperiam dolores aspernatur enim quos consequuntur corporis natus blanditiis earum eveniet nam, cum maiores ipsam repudiandae hic corrupti delectus possimus recusandae autem. Labore placeat eveniet ex sint voluptate a animi quaerat. Voluptatem, sed sit! Nihil numquam quaerat reprehenderit quas consequuntur corporis amet at perspiciatis animi labore! Similique, maiores fuga. Quas quibusdam quis velit blanditiis! Fugiat, blanditiis, iste ut quia soluta deserunt sit repellendus enim necessitatibus nihil quaerat excepturi aspernatur dolorem voluptatibus eaque nemo. Cumque ea fugiat necessitatibus dignissimos perferendis repellat tempora sunt repellendus saepe excepturi nesciunt sed, aliquid officia?
