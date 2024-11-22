91e4c6bb72bf32294681bb337cacf8f8b904f9f6
Ken W <horizonbound0@gmail.com>
2024-11-22 17:09:00 -0600

    more design tweaks

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,36 @@
 export default function Card({ projects }) {
 
-    const cardStyling = {
-        width: '18rem',
-    };
-    
     return (
         <div className="row justify-content-center">
+
             {projects.map((project) => (
-                <div className="card text-center justify-content-center m-2" style={cardStyling} key={project.title}>
-                    <img src={project.picLink} className="card-img-top w-50" alt={project.title} />
+                <div className="card text-center p-2 m-2 col" key={project.title}>
+
+                    <img src={project.picLink} className="card-img-top" alt={project.title} />
+
                     <div className="card-body">
+
                         <h5 className="card-title">{project.title}</h5>
+
+                        <p className="card-text">{project.desc}</p>
+
                         <ul className="list-group list-group-horizontal">
+
                             <li className="list-group-item">
-                                <a href={project.ghLink}>{project.title} Github</a>
+                                <a className="btn btn-primary" href={project.ghLink}>{project.title} Github</a>
                             </li>
+
                             <li className="list-group-item">
-                                <a href={project.appLink}>{project.title} Site</a>
+                                <a className="btn btn-primary" href={project.appLink}>{project.title} Site</a>
                             </li>
+
                         </ul>
+
                     </div>
+
                 </div>
             ))}
+
         </div>
 
     )
--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -8,23 +8,26 @@ export default function ProjectCards() {
             ghLink: "https://github.com/Lixiviate/P2-Book-Nest",
             appLink: "https://p2-book-nest.onrender.com/",
             picLink: "./assets/BNLogo.png",
+            desc: "If you're looking for your next literary fix, this project might help. It blended a love of books, and a love of staying home to read them. We accomplished a lot in only two weeks time.",
         },
         {
-            title: "Marvel Weather App",
+            title: "Marvel Meteorology",
             ghLink: "https://github.com/jknowles10/Marvel-Weather-App",
             appLink: "https://jknowles10.github.io/Marvel-Weather-App/",
             picLink: "./assets/MMLogo.png",
+            desc: "Ever imagine what Marvel superhero would swoop in to save a rainy day? For this project we put our new skills with using RESTful APIs to the test and combined a weather API, and the MARVEL API for some shameless comic book fun.",
         },
         {
             title: "Wizard's Apprentice: Pieces of the Master",
             ghLink: "https://github.com/nearias3/Wizard-s-Apprentice",
             appLink: "https://wizard-s-apprentice.onrender.com/",
-            picLink: "./assets/WALogo.png"
+            picLink: "./assets/WALogo.png",
+            desc: "We've all felt the pain of freezing during a zoom call, or waiting on someone else to unfreeze. But what would you do if that presenter never un-froze?? This browser game puts you, the player, on a journey to reassemble your web-master teacher after he's frozen in the middle of class! We put together everything we learned, and added some new functionality with Phaser.io.",
         },
     ]
 
     return (
-        <div className="border container  mx-auto my-2">
+        <div className="container  mx-auto my-2">
             <h1>Projects</h1>
             <div>
                 <Card projects={ projects} />
